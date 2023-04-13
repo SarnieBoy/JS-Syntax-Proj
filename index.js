70bee4474799917b077a91785dd96cbe166912ef
Addy-Chaps <123508140+SarnieBoy@users.noreply.github.com>
2023-04-13 21:42:55 +0100

    add data to arrays

---
+++ index.js
@@ -0,0 +1,8 @@
+const storyTemplate = `One day in ${townName} I saw my ${familyMember} acting ${adjectiveWord}. They were ${verbWord} a big ${thing}. They then went to the ${placeName} and did it again!`;
+const townName = ['Peterlee', 'Horden', 'Easington', 'Blackhall', 'Wingate', 'Hartlepool', 'Shotton', 'Seaton Carew', 'Seaham', 'South Hetton'];
+const familyMember = ['Mother', 'Father', 'Grandmother', 'Grandfather', 'Brother', 'Sister', 'Cousin', 'Niece', 'Nephew', 'Uncle', 'Aunt'];
+const adjectiveWord = ['Silly', 'Crazy', 'Sly', 'Tough', 'Playful', 'Angry', 'Upset', 'Happy', 'Curious', 'Casual'];
+const verbWord = ['Holding', 'Running', 'Punching', 'Riding', 'Slapping', 'Kissing', 'Grabbing', 'Scratching', 'Stroking', 'Throwing']; 
+const thing = ['Car', 'Banana', 'Spoon', 'Beer', 'Fork', 'Lemon', 'Taco', 'Watch', 'Football', 'Slipper'];
+const placeName = ['House', 'Shop', 'Town', 'Library', 'Cinema', 'Pub', 'Restaurant', 'School', 'Church', 'Bowling Alley'];
+
